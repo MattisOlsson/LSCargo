5aeba00ba2c8778f3d754030be9d8489e577d2dc
Mattias Olsson <garbageinbox@hotmail.com>
2018-09-16 12:58:58 +0200

    Working on contact page prototype.

--- src/LSCargo.Web/Controllers/PrototypeController.cs
+++ src/LSCargo.Web/Controllers/PrototypeController.cs
@@ -1,13 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LSCargo.Web.Models.Regions;
+using LSCargo.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Piranha;
+using Piranha.Data;
+using Piranha.Extend.Fields;
 
 namespace LSCargo.Web.Controllers
 {
     public class PrototypeController : Controller
     {
+        private readonly IApi _api;
+
+        public PrototypeController(IApi api)
+        {
+            _api = api;
+        }
+
         [Route("prototype/contact")]
         public IActionResult Contact()
         {
-            return View("~/Views/Prototype/Contact.cshtml");
+            Media media = _api.Media.GetAll().FirstOrDefault();
+
+            var model = new List<ContactCard>();
+            var image = new ImageField
+            {
+                Id = media.Id
+            };
+
+            image.Init(_api);
+
+            model.Add(new ContactCard
+            {
+                Image = image,
+                Name = "Jane Doe",
+                Email = "jane.doe@lscargo.com",
+                Phone = "+46701234567"
+            });
+
+            model.Add(new ContactCard
+            {
+                Image = image,
+                Name = "John Doe",
+                Email = "john.doe@lscargo.com",
+                Phone = "+46701234567"
+            });
+
+            model.Add(new ContactCard
+            {
+                Image = image,
+                Name = "Jane Doe",
+                Email = "jane.doe@lscargo.com",
+                Phone = "+46701234567"
+            });
+
+            model.Add(new ContactCard
+            {
+                Image = image,
+                Name = "John Doe",
+                Email = "john.doe@lscargo.com",
+                Phone = "+46701234567"
+            });
+
+            model.Add(new ContactCard
+            {
+                Image = image,
+                Name = "Jane Doe",
+                Email = "jane.doe@lscargo.com",
+                Phone = "+46701234567"
+            });
+
+            model.Add(new ContactCard
+            {
+                Image = image,
+                Name = "John Doe",
+                Email = "john.doe@lscargo.com",
+                Phone = "+46701234567"
+            });
+
+            return View("~/Views/Prototype/Contact.cshtml", model);
         }
     }
 }
