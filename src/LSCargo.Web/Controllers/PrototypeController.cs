6c9700d8acd96a34306be43b861c6c8ee35afa4b
Mattias Olsson <garbageinbox@hotmail.com>
2018-09-16 10:54:34 +0200

    Started on contact page prototype.

---
+++ src/LSCargo.Web/Controllers/PrototypeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LSCargo.Web.Controllers
+{
+    public class PrototypeController : Controller
+    {
+        [Route("prototype/contact")]
+        public IActionResult Contact()
+        {
+            return View("~/Views/Prototype/Contact.cshtml");
+        }
+    }
+}
